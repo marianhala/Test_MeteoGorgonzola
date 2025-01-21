806d25eaf046d6f6de0fa068b0a336369598b2a7
MarianHala <75884459+marianhala@users.noreply.github.com>
2025-01-21 14:55:50 +0100

    weatherService.js

--- weather-web-app/src/services/weatherService.js
+++ weather-web-app/src/services/weatherService.js
@@ -1,5 +1,5 @@
 export const fetchCurrentWeather = async (city) => {
-    const apiKey = '8867b17d9139fb6140dc0d31c117588c';
+    const apiKey = 'YourKey';
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`);
     if (!response.ok) {
         throw new Error('Failed to fetch current weather data');
@@ -8,7 +8,7 @@ export const fetchCurrentWeather = async (city) => {
 };
 
 export const fetch5DayForecast = async (city) => {
-    const apiKey = '8867b17d9139fb6140dc0d31c117588c';
+    const apiKey = 'YourKey';
     const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`);
     if (!response.ok) {
         throw new Error('Failed to fetch 5-day forecast data');
@@ -17,7 +17,7 @@ export const fetch5DayForecast = async (city) => {
 };
 
 export const fetchAirQuality = async (lat, lon) => {
-    const apiKey = '8867b17d9139fb6140dc0d31c117588c';
+    const apiKey = 'YourKey';
     const response = await fetch(`https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${apiKey}`);
     if (!response.ok) {
         throw new Error('Failed to fetch air quality data');
